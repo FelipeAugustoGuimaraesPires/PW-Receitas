5bea36ea4795e5e26e09dba6aa32c302e0432ce0
GabrielAron <103843849+GabrielAron@users.noreply.github.com>
2023-04-05 22:02:22 -0300

    PW_0.6

--- Codigo/Pw-Projeto/index.js
+++ Codigo/Pw-Projeto/index.js
@@ -1,27 +1,55 @@
-var button = document.getElementById('botao_cadastro');
+const button = document.getElementById('botao_cadastro');
 const popup = document.querySelector('.popup_wrapper');
 const closeButton = document.querySelector('.popup_close');
 
 
 button.addEventListener('click', abrirPopup)
 
-closeButton.addEventListener('click',()=> {
-    popup.style.display='none';
+closeButton.addEventListener('click', () => {
+    popup.style.display = 'none';
 })
 
-function abrirPopup(){
-   
-    alert("Você clicou na div!");
+function abrirPopup() {
+    popup.style.display = 'block';
+}
+
+function mostrarSenha() {
+    var inputPass = document.getElementById('senha')
+    var btnShowPass = document.getElementById('olhoaberto')
+
+    if (inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
+    } else {
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
+    }
 
 }
 
-/*<div id="minha-div">Clique aqui</div>
+popup.addEventListener('click',()=>{
+    modal.classList.toggle('animar');
+})
 
-<script>
-  function minhaFuncao() {
-    alert("Você clicou na div!");
-  }
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(this.mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activaClass = 'active';
 
-  var minhaDiv = document.getElementById("minha-div");
-  minhaDiv.addEventListener("click", minhaFuncao);
-</script>*/
+        this.handleClick = this.handleClick.bind();
+
+    }
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("Hey"))
+           
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
